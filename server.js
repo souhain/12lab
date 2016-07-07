362ee36664dcdf115e2cc1d6e876e6f652c66c5d
Edgar Paredes <edgarparedes283@gmail.com>
2016-07-07 09:58:27 -0500

    Lab05 HTML fijo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //  http
 var http = require("http");
+var fs = require('fs');
 // obteniendo informacion del entorno de ejecucion 
 //con respecto al ip y al puerto que debemos usar en nuestro server
 var PORT = process.env.PORT || 3000;
@@ -13,13 +14,22 @@ var server  = http.createServer(function (req,res){
     // Armar respuesta http 
     // Armar un encabezado hhtp
     res.writeHead(200,{
-        "content-Type":"text/plain",
+        "content-Type":"text/html",
         "server":"ITGAM@4.2.4"
     });
-    // Enviamos la respuesta
-    res.write(" EDGAR ");
-    // cerrar la conexion
-    res.end();
+    //lectura del archivo a servir
+    fs.readFile(
+        './static/index.html',
+        'utf8',function (err,content) {
+            if(err){
+                res.end("<h1>ERROR DE LECTURA</h1>");
+            }
+            else
+            {
+                res.end(content);
+            }
+        });
+    
 });
 // Poner a trabajar al server
  server.listen(PORT,IP,function () {
