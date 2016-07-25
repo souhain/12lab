c3386e5595504dd9eba093755be83e09492e1a50
Edgar Paredes <edgarparedes283@gmail.com>
2016-07-25 09:16:55 -0500

    se conecta a base de datos pero mensaje vacio

--- internals/fortune.js
+++ internals/fortune.js
@@ -1,14 +1,39 @@
+// cargando el driver de mongodb que permitira conectarme a la base de datos
+var mongodb = require("mongodb");
+// cargando el cliente del driver
+var mongoClient = mongo.mongoClient;
+
+
 var fortunePaper = {
-    "mensaje":
-    "La honestidad es un regalo caro, no lo esperes de gente barata"
+
 };
 module.exports = {
     "getFortune": function (cb) {
-        // Convirtiendo el fortunePaper de objeto
-        // a su version en string
-       var fortunePaperResponse = JSON.stringify(fortunePaper);
-        // Ejecuto el callback (cb) pasandole
-        // como parametro el fortunepaper string
-        cb(fortunePaperResponse);
+        // conectando al cliente a la BD fortune
+        mongoClient.connect("mongodb://127.0.0.1:27017/fortune",
+            function (err, db) {
+                if (err) {
+                    console.log("> Error al conectarse a base de datos"
+                        + "mongodb://127.0.0.1:27017/fortune");
+                    var fortunePaper = {
+                        "mensaje":
+                        "La honestidad es un regalo caro, no lo esperes de gente barata"
+                    };
+                    var fortunePaperResponse = JSON.stringify(fortunePaper);
+             cb(fortunePaperResponse);   
+            }
+            else{
+                var paperCollection = db.paperCollection('paper');
+                var objetoResultado = paperCollection.find({});
+                objetoResultado.toArray(function(err, papers){
+                    var fortunePaperResponse=
+                    JSON.stringify(paper[0]);
+                    db.close();
+                    cb(fortunePaperResponse);
+                })               
+            }
+            });
+
+        
     }
 };
